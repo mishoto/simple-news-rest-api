ea70e469ed2f8a596c7df113a8a3826fbc9cfe0a
mishoto <mihail.dev.io@gmail.com>
2022-11-10 00:48:28 +0200

    add dbConfig & routes to path

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const Koa = require('koa');
-const Router = require("@koa/router")();
 const session = require("koa-session");
 const json = require("koa-json");
+const bodyParser = require("koa-bodyparser");
+const serve = require("koa-static")
+
+const {routes, allowedMethods} = require("./routes/newsRoutes")
 
 const app = new Koa();
-const router = new Router();
+const dbConfig = require("./configs/dbConfig")
 
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(routes());
+app.use(allowedMethods());
 app.use(session(app));
+app.use(bodyParser());
 app.use(json());
-
-// app.use(koaBody);
+app.use(serve("./public"))
+app.use(dbConfig.Mongoose);
 
 module.exports = app;
+
