dafcaaf9f1700aaafd89a0a04295f64f5d56b1d0
mishoto <mihail.dev.io@gmail.com>
2022-11-10 23:35:54 +0200

    move .env config call to app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+
 const Koa = require('koa');
 const session = require("koa-session");
 const json = require("koa-json");
 const bodyParser = require("koa-bodyparser");
 const serve = require("koa-static")
-
 const {routes, allowedMethods} = require("./routes/newsRoutes")
+require('dotenv').config({debug: true})
 
 const app = new Koa();
 const dbConfig = require("./configs/dbConfig")
@@ -15,7 +16,8 @@ app.use(session(app));
 app.use(bodyParser());
 app.use(json());
 app.use(serve("./public"))
-app.use(dbConfig.Mongoose);
+app.use(dbConfig.Mongoose)
+    // .db("news-app").collection("news");
 
 module.exports = app;
 
